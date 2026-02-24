e24d62b72b93fb323c9bcbb075d4758bd92a560b
Siryll <50954074+Siryll@users.noreply.github.com>
2026-02-24 16:46:41 +0100

    Feature/http client (#15)
    * implement http client
    
    * use alloc to allow for bigger responses from requests

--- src/wifi/mod.rs
+++ src/wifi/mod.rs
@@ -1,11 +1,11 @@
-use defmt::info;
+use defmt::{debug, info, warn};
 use embassy_executor::Spawner;
 use embassy_net::{Runner, Stack, StackResources};
 use embassy_time::{Duration, Timer};
 use esp_alloc as _;
 use esp_hal::rng::Rng;
 use esp_radio::wifi::{
-    ClientConfig, ModeConfig, ScanConfig, WifiController, WifiDevice, WifiEvent,
+    ClientConfig, ModeConfig, ScanConfig, WifiController, WifiDevice, WifiEvent, WifiStaState,
 };
 
 #[allow(
@@ -49,14 +49,15 @@ impl Wifi {
         let config = embassy_net::Config::dhcpv4(Default::default());
 
         let rng = Rng::new();
-        let seed = (rng.random() as u64) << 32 | rng.random() as u64;
+        let net_seed = rng.random() as u64 | ((rng.random() as u64) << 32);
+        let tls_seed = rng.random() as u64 | ((rng.random() as u64) << 32);
 
         // Init network stack
         let (stack, runner) = embassy_net::new(
             wifi_interface,
             config,
-            mk_static!(StackResources<3>, StackResources::<3>::new()),
-            seed,
+            mk_static!(StackResources<8>, StackResources::<8>::new()),
+            net_seed,
         );
 
         // configure wifi with credentials
@@ -72,16 +73,33 @@ impl Wifi {
         spawner.spawn(connection(controller)).ok();
         spawner.spawn(net_task(runner)).ok();
 
-        Self {
-            stack,
-            tls_seed: seed,
-        }
+        Self { stack, tls_seed }
+    }
+
+    pub fn stack(&self) -> Stack<'static> {
+        self.stack
+    }
+
+    pub fn tls_seed(&self) -> u64 {
+        self.tls_seed
     }
 
     pub async fn wait_for_connection(&self) {
-        self.stack.wait_config_up().await;
-        if let Some(config) = self.stack.config_v4() {
-            info!("Got IP: {}", config.address);
+        info!("Waiting for link to be up");
+        loop {
+            if self.stack.is_link_up() {
+                break;
+            }
+            Timer::after(Duration::from_millis(500)).await;
+        }
+
+        info!("Waiting to get IP address...");
+        loop {
+            if let Some(config) = self.stack.config_v4() {
+                info!("Got IP: {}", config.address);
+                break;
+            }
+            Timer::after(Duration::from_millis(500)).await;
         }
     }
 }
@@ -90,40 +108,36 @@ impl Wifi {
 #[embassy_executor::task]
 async fn connection(mut controller: WifiController<'static>) {
     info!("start connection task");
-
     loop {
+        if esp_radio::wifi::sta_state() == WifiStaState::Connected {
+            // wait until we're no longer connected
+            controller.wait_for_event(WifiEvent::StaDisconnected).await;
+            Timer::after(Duration::from_millis(5000)).await
+        }
         if !matches!(controller.is_started(), Ok(true)) {
             info!("Starting wifi");
             controller.start_async().await.unwrap();
             info!("Wifi started!");
 
-            info!("Scan");
+            debug!("Scan");
             let scan_config = ScanConfig::default().with_max(10);
             let result = controller
                 .scan_with_config_async(scan_config)
                 .await
                 .unwrap();
             for ap in result {
-                info!("{:?}", ap);
+                debug!("{:?}", ap);
             }
         }
-
         info!("About to connect...");
 
         match controller.connect_async().await {
-            Ok(()) => {
-                info!("Wifi connected");
-
-                // wait until we're no longer connected
-                let disconnect_reason = controller.wait_for_event(WifiEvent::StaDisconnected).await;
-                info!("Disconnected: {:?}", disconnect_reason);
-            }
+            Ok(_) => info!("Wifi connected!"),
             Err(e) => {
-                info!("Failed to connect to wifi: {:?}", e);
+                warn!("Failed to connect to wifi: {:?}", e);
+                Timer::after(Duration::from_millis(5000)).await
             }
         }
-
-        Timer::after(Duration::from_millis(5000)).await
     }
 }
 
