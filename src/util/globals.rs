158e546beafe5cd1aa0fdc05b5dc8f673c802f6e
Siryll <50954074+Siryll@users.noreply.github.com>
2026-02-27 15:34:24 +0100

    add widget and globals(#17)
    add widget modules with needed changes to storage, make storage and http a static global to be able to access them from anywhere without needing to pass context

---
+++ src/util/globals.rs
@@ -0,0 +1,53 @@
+use crate::http_client::EspHttpClient;
+use crate::storage::Storage;
+use embassy_net::Stack;
+use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
+use embassy_sync::mutex::Mutex;
+
+type GlobalMutex<T> = Mutex<CriticalSectionRawMutex, Option<T>>;
+
+static STORAGE: GlobalMutex<Storage<'static>> = Mutex::new(None);
+
+static mut NETWORK_STACK: Option<Stack<'static>> = None;
+
+static mut TLS_SEED: Option<u64> = None;
+
+pub async fn init_storage(storage: Storage<'static>) {
+    let mut guard = STORAGE.lock().await;
+    if guard.is_some() {
+        panic!("Storage already initialized!");
+    }
+    *guard = Some(storage);
+    defmt::info!("Global storage initialized");
+}
+
+pub async fn with_storage<F, R>(f: F) -> R
+where
+    F: FnOnce(&mut Storage<'static>) -> R,
+{
+    let mut guard = STORAGE.lock().await;
+    let storage = guard
+        .as_mut()
+        .expect("Storage not initialized! Call init_storage() first");
+    f(storage)
+}
+
+pub fn init_network(stack: Stack<'static>, tls_seed: u64) {
+    unsafe {
+        NETWORK_STACK = Some(stack);
+        TLS_SEED = Some(tls_seed);
+    }
+    defmt::info!("Global network stack initialized");
+}
+
+pub fn network_stack() -> Stack<'static> {
+    unsafe { NETWORK_STACK.expect("Network not initialized! Call init_network() first") }
+}
+
+pub fn tls_seed() -> u64 {
+    unsafe { TLS_SEED.expect("Network not initialized! Call init_network() first") }
+}
+
+pub fn http_client() -> EspHttpClient {
+    EspHttpClient::new(network_stack(), tls_seed())
+}
