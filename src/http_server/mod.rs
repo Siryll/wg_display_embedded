a26756dad3f032c7129f04bf87dac774da0ce0b2
Siryll <zumofencyril@gmail.com>
2026-03-04 13:02:58 +0100

    add implementation for endpoints & move buffers to heap

--- src/http_server/mod.rs
+++ src/http_server/mod.rs
@@ -1,15 +1,21 @@
 use embassy_net::Stack;
 use embassy_time::Duration;
 use esp_alloc as _;
+use esp_hal::ledc::channel::config;
 use picoserve::{
     AppBuilder, AppRouter, Router, 
     response::{File, IntoResponse, Json}, 
     routing::{self, parse_path_segment}
 };
 use embassy_executor::Spawner;
-use defmt::{info, warn};
+use defmt::{info, warn, error};
 
-pub const WEB_TASK_POOL_SIZE: usize = 2;
+use crate::{common::models::{InstallAction, SystemConfiguration}, util::globals, widget::store::WidgetStore};
+use crate::common::models::WidgetStoreItem;
+use crate::widget::manager::WidgetManager;
+use alloc::format;
+
+pub const WEB_TASK_POOL_SIZE: usize = 1;
 
 pub struct Application;
 
@@ -23,7 +29,7 @@ impl AppBuilder for Application {
         )
         .route("/get_store_items", routing::get(get_store_items))
         .route("/install_widget", routing::post(post_install_widget))
-        .route("system_config", routing::post(post_system_config))
+        .route("/system_config", routing::post(post_system_config))
         .route("/system_config", routing::get(get_system_config))
         // "/deinstall_widget/<widget_name>"
         .route(
@@ -51,41 +57,126 @@ impl AppBuilder for Application {
 // endpoint implementations (stubs until common is implemented)
 
 async fn get_store_items() -> impl IntoResponse {
-    warn!("get_store_items endpoint called, not yet implemented");
-    "Not implemented yet"
+    let mut store = WidgetStore::new();
+    store.fetch_from_store().await.map_err(|err| {
+        error!("Failed to fetch widget store: {:?}", err);
+    }).ok();
+    Json(store.get_items().to_vec())
 }
 
 async fn get_system_config() -> impl IntoResponse {
-    warn!("get_system_config endpoint called, not yet implemented");
-    "Not implemented yet"
+    let res = globals::with_storage(|storage| {
+        storage.get_widget_config()
+    }).await;
+    match res {
+        Ok(config) => Json(config),
+        Err(err) => {
+            error!("Failed to load system config: {:?}", err);
+            Json(SystemConfiguration::default())
+        }
+    }
 }
 
-async fn post_install_widget() -> impl IntoResponse {
-    warn!("install_widget endpoint called, not yet implemented");
-    "Not implemented yet"
+async fn post_install_widget(
+    Json(action) : Json<InstallAction>
+) -> impl IntoResponse {
+    let (download_url, description) = match action {
+        InstallAction::FromUrl(url) => (url, alloc::string::String::from("No description")),
+        InstallAction::FromStoreItemName(name) => {
+            let mut store = WidgetStore::new();
+            if let Err(err) = store.fetch_from_store().await {
+                error!("Failed to fetch widget store before install: {:?}", err);
+                return Err(());
+            }
+            let item: &WidgetStoreItem = store
+                .get_items()
+                .iter()
+                .find(|item: &&WidgetStoreItem| item.name == name)
+                .unwrap();
+            (item.get_download_url(), item.description.clone())
+        }
+    };
+    info!("Installing widget from URL {}", download_url.as_str());
+    let result = WidgetManager::install_widget(download_url.as_str(), &description).await;
+    match result {
+        Ok(_) => Ok(()),
+        Err(err) => {
+            error!("{}", err);
+            // TODO: add proper errors
+            Err(())
+        }
+    }
 }
 
-async fn post_system_config() -> impl IntoResponse {
-    warn!("post_system_config endpoint called, not yet implemented");
-    "Not implemented yet"
+async fn post_system_config(
+    Json(config): Json<SystemConfiguration>
+) -> impl IntoResponse {
+    let result =globals::with_storage(|storage| {
+        storage.save_widget_config(&config)
+    }).await;
+    match result {
+        Ok(_) => Ok(()),
+        Err(err) => {
+            error!("Failed to save system config: {:?}", err);
+            Err(())
+        }
+    }
 }
 
 async fn deinstall_widget(widget_name: alloc::string::String) -> impl IntoResponse {
-    warn!("deinstall_widget endpoint called for widget {}, not yet implemented", widget_name.as_str());
-    "Not implemented yet"
+    let result = WidgetManager::deinstall_widget(widget_name.as_str()).await;
+    match result {
+        Ok(_) => Ok(()),
+        Err(err) => {
+            error!("Failed to deinstall widget {}: {:?}", widget_name.as_str(), err);
+            Err(())
+        }
+    }
 }
 
 async fn get_config_schema(widget_name: alloc::string::String) -> impl IntoResponse {
+    // TODO: requires runtime implementation
     warn!("get_config_schema endpoint called for widget {}, not yet implemented", widget_name.as_str());
     "Not implemented yet"
 }
 
-async fn post_widget_config(widget_name: alloc::string::String) -> impl IntoResponse {
-    warn!("post_widget_config endpoint called for widget {}, not yet implemented", widget_name.as_str());
-    "Not implemented yet"
+async fn post_widget_config(
+    widget_name: alloc::string::String,
+    Json(config): Json<alloc::string::String>
+) -> impl IntoResponse {
+    let mut system_config = match globals::with_storage(|storage| storage.get_widget_config()).await {
+        Ok(system_config) => system_config,
+        Err(err) => {
+            error!(
+                "Could not load system config to save widget config for widget {}: {}",
+                widget_name.as_str(),
+                err
+            );
+            return Err(());
+        }
+    };
+
+    system_config.widgets.iter_mut().for_each(|widget| {
+        if widget.name == widget_name {
+            widget.json_config = config.clone();
+        }
+    });
+
+    match globals::with_storage(|storage| storage.save_widget_config(&system_config)).await {
+        Ok(_) => Ok(()),
+        Err(err) => {
+            error!(
+                "Could not save widget config for widget {}: {}",
+                widget_name.as_str(),
+                err
+            );
+            Err(())
+        }
+    }
 }
 
 async fn get_widget_config(widget_name: alloc::string::String) -> impl IntoResponse {
+    // TODO: requires frontend implementation
     warn!("get_widget_config endpoint called for widget {}, not yet implemented", widget_name.as_str());
     "Not implemented yet"
 }
@@ -141,12 +232,12 @@ pub async fn web_task(
     config: &'static picoserve::Config,
 ) -> ! {
     let port = 80;
-    let mut tcp_rx_buffer = [0; 1024];
-    let mut tcp_tx_buffer = [0; 1024];
-    let mut http_buffer = [0; 2048];
+    let mut tcp_rx_buffer = alloc::boxed::Box::new([0u8; 1024]);
+    let mut tcp_tx_buffer = alloc::boxed::Box::new([0u8; 1024]);
+    let mut http_buffer = alloc::boxed::Box::new([0u8; 2048]);
 
-    picoserve::Server::new(router, config, &mut http_buffer)
-        .listen_and_serve(task_id, stack, port, &mut tcp_rx_buffer, &mut tcp_tx_buffer)
+    picoserve::Server::new(router, config, &mut *http_buffer)
+        .listen_and_serve(task_id, stack, port, &mut *tcp_rx_buffer, &mut *tcp_tx_buffer)
         .await
         .into_never()
 }
--- src/main.rs
+++ src/main.rs
@@ -65,8 +65,9 @@ async fn main(spawner: Spawner) -> ! {
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let peripherals = esp_hal::init(config);
 
-    esp_alloc::heap_allocator!(#[esp_hal::ram(reclaimed)] size: 73744);
     esp_alloc::psram_allocator!(peripherals.PSRAM, esp_hal::psram);
+    esp_alloc::heap_allocator!(#[esp_hal::ram(reclaimed)] size: 73744);
+    
 
     let timg0 = TimerGroup::new(peripherals.TIMG0);
     esp_rtos::start(timg0.timer0);
