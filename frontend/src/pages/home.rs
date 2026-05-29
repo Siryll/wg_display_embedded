64fcdfd74bbb31b6f72486c2f1aaa4224d2a719e
Siryll <50954074+Siryll@users.noreply.github.com>
2026-05-29 09:59:51 +0200

    fmt fix in frontend

--- frontend/src/pages/home.rs
+++ frontend/src/pages/home.rs
@@ -18,7 +18,6 @@ use crate::routing::router::Route;
 pub fn home() -> Html {
     let system_config = use_reducer(SystemConfiguration::default);
     let error = use_state(|| None as Option<String>);
-    
 
     {
         // Only runs on first render
